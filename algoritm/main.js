97173508146abdbec1023df6110e683c875988e9
babak-1 <babek.taghiyev@gmail.com>
2024-03-15 20:25:08 +0400

    countNegativeNumber

--- algoritm/main.js
+++ algoritm/main.js
@@ -1,4 +1,4 @@
-// [8,4,6,2,3] => [4,2,4,2,3]
+//1) [8,4,6,2,3] => [4,2,4,2,3]
 
 // menim yazdigim
 
@@ -45,6 +45,33 @@ const solution = (arr) =>{
 console.log(solution([8,4,6,2,3]));
 console.log(solution([8,8,1,3,2])); 
 
+
+
+// 2) [[4,3,2,-1],[3,-2,-1,6],[5,-4,0,-1]]  menfi ededlerin sayi => 5
+
+
+function negativeNumber(arr){
+    let number = 0;
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+            if(arr[i][j]<0){
+                number++;
+            }
+        }
+    }
+
+    return number;
+
+}
+
+// alternativ yol
+console.log(negativeNumber([[4,3,2,-1],[3,-2,-1,6],[5,-4,0,-1]]));
+
+function negativeNumberAlt(arr){
+    return arr.map(item =>item.filter(r => r<0).length).reduce((a,b) => a+b,0)
+}
+
+console.log(negativeNumberAlt([[4,3,2,-1],[3,-2,-1,6],[5,-4,0,-1]]));
 
 
 
